028fb818e2cfe61f74dfda8d754528c74afdcb58
korolenkoae <korolenkoae@mail.ru>
2023-04-19 22:20:59 -0700

    Добавила тест  на получение списка игроков

--- utils/api/api.py
+++ utils/api/api.py
@@ -10,6 +10,7 @@ class Api:
     api_baseurl = "http://localhost:8000/"
     API_CLUBS = api_baseurl + "api/clubs"
     API_PLAYERS = api_baseurl + "api/players"
+    API_CLUB_PLAYERS = api_baseurl + "api/players/players_by_club/"
 
     @staticmethod
     @allure.step("Отправляем GET запрос списка игровых серверов")
@@ -73,7 +74,7 @@ class Api:
     def post_new_club(club_name, city):
         with allure.step(f"Запроса на создание клуба{city, club_name}"):
             result = HttpManager.post(
-                Api.API_CLUBS,
+                Api.API_CLUBS + "/",
                 JSONFixture.for_post_clubs(city, club_name),
             )
             Api.LOGGER.info(
@@ -105,3 +106,76 @@ class Api:
                 pass
             return result
 
+    @staticmethod
+    def put_clubs(put_id, club_name, city):
+        with allure.step(f"Обновление клуба с {put_id, club_name, city}"):
+            result = HttpManager.put(
+                Api.API_CLUBS + "/" + str(put_id) + "/",
+                JSONFixture.for_put_clubs(put_id, club_name, city),
+            )
+            Api.LOGGER.info(
+                "TEST: Put News data. Method: {0}, Data: {1}".format(
+                    "PUT",
+                    JSONFixture.for_put_clubs(put_id, club_name, city),
+                )
+            )
+            with allure.step(
+                f"Успешное обновление клуба с put_id"
+                f"{put_id, club_name, city} {result.json()}"
+            ):
+                pass
+            return result
+
+    @staticmethod
+    def put_players(put_id_player, nickname, club, name):
+        with allure.step(f"Обновление пользователя с {put_id_player, nickname, club, name}"):
+            result = HttpManager.put(
+                Api.API_PLAYERS + "/" + str(put_id_player) + "/",
+                JSONFixture.for_put_players(put_id_player, nickname, club, name),
+            )
+            Api.LOGGER.info(
+                "TEST: Put News data. Method: {0}, Data: {1}".format(
+                    "PUT",
+                    JSONFixture.for_put_players(put_id_player, nickname, club, name),
+                )
+            )
+            with allure.step(
+                    f"Успешное обновление пользователя с put_id_player"
+                    f"{put_id_player, nickname, club, name} {result.json()}"
+            ):
+                pass
+            return result
+
+    @staticmethod
+    def delete_players(id_player):
+        with allure.step(f"Удаление пользователя с {id_player}"):
+            result = HttpManager.delete(Api.API_PLAYERS + "/" + str(id_player))
+            Api.LOGGER.info("TEST: Put News data. Method: {0}, Data: {1}")
+            with allure.step(
+                    f"Успешное удаление пользователя с id_player"
+                    f"{id_player} {result.json()}"
+            ):
+                pass
+            return result
+
+    @staticmethod
+    def delete_clubs(id_club):
+        with allure.step(f"Удаление клуба с {id_club}"):
+            result = HttpManager.delete(Api.API_CLUBS + "/" + str(id_club))
+            Api.LOGGER.info("TEST: Put News data. Method: {0}, Data: {1}")
+            with allure.step(
+                    f"Успешное удаление клуба с id_club"
+                    f"{id_club} {result.json()}"
+            ):
+                pass
+            return result
+
+    @staticmethod
+    @allure.step("Отправляем GET запрос списка игроков клуба")
+    def get_club_players(club_name):
+        with allure.step("Получение списка игроков клуба"):
+            result = HttpManager.get(Api.API_CLUB_PLAYERS + club_name)
+            Api.LOGGER.info("TEST: Получение списка игроков. Method: {0}, Data: {1}")
+            with allure.step(f"Успешное получение списка игроков клуба {result.json()}"):
+                pass
+            return result
